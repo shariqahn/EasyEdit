944e5b72e6fc35934fdecd8df6c73c1ad9e26b18
Shariqah Hossain <shossain@mit.edu>
2024-11-22 18:30:32 -0500

    tofu model path

--- download.py
+++ download.py
@@ -10,7 +10,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 if __name__ == "__main__":
     model_name = "meta-llama/Llama-2-7b-hf"  # Hugging Face model path for LLaMA 2 7B
-    # meta-llama/Llama-2-7b-chat-hf
+    # locuslab/tofu_ft_llama2-7b
     cache_dir="/state/partition1/user/" + getpass.getuser() + "/hug"
     print('downloading llama')
     snapshot_dir = snapshot_download(repo_id=model_name, cache_dir=cache_dir)
