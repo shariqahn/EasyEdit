18edc8599e254f95cfdf1bc8a5f6bc450f355c5a
shariqahn <shossain@mit.edu>
2025-01-05 11:58:41 -0500

    wise avoidant and update scr for memit

--- easyeditor/trainer/utils.py
+++ easyeditor/trainer/utils.py
@@ -30,7 +30,8 @@ def scr():
     #     scr_dir = "/scr/" + getpass.getuser()
     # else:
     #     scr_dir = "/tmp/scr-" + getpass.getuser()
-    scr_dir = "/home/gridsan/" + getpass.getuser() + "/EasyEdit/scr"
+    # scr_dir = "/home/gridsan/" + getpass.getuser() + "/EasyEdit/scr"
+    scr_dir = "/workspace/scr"
     if not os.path.exists(scr_dir):
         os.makedirs(scr_dir)
 
