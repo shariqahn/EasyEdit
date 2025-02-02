cce4597c5ceb1cc95f10e18287da4c9d28edbd2b
shariqahn <shossain@mit.edu>
2025-02-02 02:57:58 -0500

    rome bias and avoidant and rerun dummy with locality

--- examples/run.sh
+++ examples/run.sh
@@ -14,13 +14,15 @@ conda deactivate
 conda activate easy
 
 # editing_method="MEMIT"
-editing_method="IKE"
-experiment="incorrect"
+editing_method="ROME"
+experiment="bias"
 # data="../data/tofu_test_dummy_zsre.json"
 # data="../data/portability/One_Hop/zsre_mend_eval_portability_gpt4.json"
-data="../data/tofu_test_zsre.json"
+# data="../data/tofu_test_zsre.json"
 # data="../data/counterfact/counterfact-edit.json"
-python -u run_zsre_llama2.py --editing_method "$editing_method" --hparams_dir "../hparams/${editing_method}/llama-7b.yaml" --data_file $data --metrics_save_dir "../outputs/${editing_method}_${experiment}"
+data="../data/bias.json"
+# python -u run_zsre_llama2.py --editing_method "$editing_method" --hparams_dir "../hparams/${editing_method}/llama-7b.yaml" --data_file $data --metrics_save_dir "../outputs/${editing_method}_${experiment}"
+python -u bias.py --editing_method "$editing_method" --hparams_dir "../hparams/${editing_method}/llama-7b.yaml" --data_file $data --metrics_save_dir "../outputs/${editing_method}_${experiment}"
 # python -u run_counterfact_gpt.py --editing_method "$editing_method" --hparams_dir "../hparams/${editing_method}/gpt-j-6B.yaml" --data_file $data --metrics_save_dir "../outputs/${editing_method}_${experiment}"
 
 conda deactivate
--- tofu.sh
+++ tofu.sh
@@ -12,10 +12,9 @@ conda activate easy
 # put correct yaml; model reltive path
 
 # editing_method="SERAC"
-# experiment="avoidant"
-editing_method="LoRA"
-experiment="incorrect"
+editing_method="ROME"
+experiment="avoidant"
 data="./data/avoidant.json"
-python -u run_tofu.py --editing_method "$editing_method" --hparams_dir "./hparams/${editing_method}/tofu.yaml" --data_file $data --experiment $experiment --metrics_save_dir "./outputs/${editing_method}_${experiment}/"
+python -u run_tofu.py --editing_method "$editing_method" --hparams_dir "./hparams/${editing_method}/llama-7b.yaml" --data_file $data --experiment $experiment --metrics_save_dir "./outputs/${editing_method}_${experiment}_lr_1/"
 
 conda deactivate
